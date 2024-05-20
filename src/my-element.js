3075be0922de6d9b8781cee5e6c37c3342d4cb04
simonmantillago <150193274+simonmantillago@users.noreply.github.com>
2024-05-20 11:37:44 -0500

    Images :feat:

--- src/my-element.js
+++ src/my-element.js
@@ -31,21 +31,6 @@ export class MyElement extends LitElement {
 
   render() {
     return html`
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
     `
   }
 
