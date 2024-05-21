e3c31cc7de6c85c6a1048b1b983f2e29b03e4f69
jorgejaimesg <158189835+Jorgejaimesg@users.noreply.github.com.>
2024-05-20 19:25:35 -0500

    feat :construction: just left the syle of the web component

--- src/modules/pages-info.js
+++ src/modules/pages-info.js
@@ -1,15 +1,67 @@
 import data from '../pages.json';
-import { LitElement, css, html } from 'lit'
+import { LitElement, css, html } from 'lit';
 
 
 
 export class pageOption extends LitElement{
+
+
+    constructor() {
+        super();
+        this.page = ''; // Inicializa la propiedad name
+    };
+    static properties = {
+        page: { type: String } // lee la propiedad page que se pone en el web component
+    };
+    //estilos de cada web component
+    static styles = css` 
+    img{
+        width:100%
+    }
+    .cards-container{
+        padding:5%;
+        width:100%;
+    }
+    .cards-container,.card{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap:20px;
+        transition: transform 0.3s ease;
+    }
+    .card{
+        font-size: 5vw;
+        text-align:center;
+        padding:5%
+    }
+    .card:hover{
+        background-color: rgba(48, 48, 48, 0.814);
+        transform: translateY(-10px);
+    }
+    @media (min-width:600px) {
+        .cards-container{
+            flex-direction: row;
+            justify-content:center;
+        }
+        .card{
+            font-size: clamp(1em,2.5vw,1.5em);
+        }
+        
+    }
+    `
+
     render(){
+        let pageData=data[this.page]; // se define el page data con la info de solo la pagina que se necesita 
         return html`
             <div class="cards-container" >
-                <img src="../${data['page-1']['1.1'].url}" alt="picture">
-                <p style="color:white;">${data['page-1']['1.1'].txt}</p>
+            ${Object.values(pageData).map(item => html`    
+                <div class="card">
+                    <img src="../${item.url}" alt="picture">
+                    <p style="color:white;">${item.txt}</p>
+                </div>
+            `)}
             </div>`
-    }
+    } //itera con el map para todas las opciones que hay dentro de page y realiza una card por cada una
 }
-customElements.define("page-option",pageOption)
+customElements.define("page-option",pageOption);
